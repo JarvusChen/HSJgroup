b6a4cc92c074e57b8128ebad6f78291541d0de8b
JarvusChen <yiwen1210.chen@gmail.com>
2016-04-27 16:02:24 +0900

    Finished Shape superclass

--- HSJgroup/src/Shape.java
+++ HSJgroup/src/Shape.java
@@ -1,25 +1,25 @@
 
 abstract public class Shape {
-	private double radius;
-
-//	getter and setter
-	public double getRadius() {
-		return radius;
-	}
-
-	public void setRadius(double radius) {
-		this.radius = radius;
-	}
-
+	private double id;
+	
 //	constructor
-	public Shape(double radius) {
+	public Shape(double id) {
 		super();
-		this.radius = radius;
+		this.id = id;
 	}
 	
-	
-//	
-	
-	
+//	getter and setter
+	public double getId() {
+		return id;
+	}
+
+	public void setId(double id) {
+		this.id = id;
+	}
+
+
+	public abstract double calculateArea();
+	public abstract double calculatePerimeter();
+	public abstract void printinfo();
 	
 }
