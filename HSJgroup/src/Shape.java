279b0ca4c9316f77668fa23a87348594808a8d65
JarvusChen <yiwen1210.chen@gmail.com>
2016-04-27 15:53:19 +0900

    1

---
+++ HSJgroup/src/Shape.java
@@ -0,0 +1,25 @@
+
+abstract public class Shape {
+	private double radius;
+
+//	getter and setter
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+//	constructor
+	public Shape(double radius) {
+		super();
+		this.radius = radius;
+	}
+	
+	
+//	
+	
+	
+	
+}
